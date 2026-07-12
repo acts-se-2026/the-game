5f960e7fbd7ab47092f2037870ac5efc914c2a6a
Daniel Giacobelli <114685295+Daniel-Gia@users.noreply.github.com>
2026-07-12 11:34:51 +0200

    Add pytest

---
+++ src/backend/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+@pytest.fixture(scope="package")
+def client():
+    with TestClient(app) as c:
+        yield c
