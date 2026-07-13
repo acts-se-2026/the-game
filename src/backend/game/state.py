a0f3f7686878e98f48e5fd74d76ac7e1ce0a7e65
Mikołaj Żuber <mikolajzuber@gmail.com>
2026-07-13 11:44:41 +0200

    Update game state code to use the new vector class

--- src/backend/game/state.py
+++ src/backend/game/state.py
@@ -1,3 +1,5 @@
+from vector import Vec2
+
 # rotations:  0 is up, value in radians
 # positions:   (0, 0) is top-left, value in pixels
 
@@ -8,14 +10,12 @@ class Box:
 
 
     def area_colliding(box1, box2):
-        x1, y1 = box1.pos
-        x2, y2 = box2.pos
         return not (
-            (x1 + box1.size[0] < x2) # x1 is too much to the left, so it doesn't collide
-            or (x1 > x2 + box2.size[0]) # x1 is too much to the right, so it doesn't collide
+            (box1.x + box1.size.x < box2.x) # x1 is too much to the left, so it doesn't collide
+            or (box1.x > box2.x + box2.size.x) # x1 is too much to the right, so it doesn't collide
         ) or (
-            (y1 + box1.size[1] < y2) # y1 is too far down, so it doesn't collide
-            or (y1 > y2 + box2.size[1]) # y1 is too far up, so it doesn't collide
+            (box1.y + box1.size.y < box2.y) # y1 is too far down, so it doesn't collide
+            or (box1.y > box2.y + box2.size.y) # y1 is too far up, so it doesn't collide
         )
 
 
@@ -31,7 +31,7 @@ class Player:
 
         self.box = Box(self.pos, 10)
 
-        self.movement = (0, 0)
+        self.movement_dir = Vec2(0, 0)
     
     def kill(self):
         self.alive = False
@@ -51,9 +51,9 @@ class Bullet:
 
     def __init__(self, pos):
         self.pos = pos
-        self.movement = (0, 0)
+        self.movement_dir = Vec2(0, 0)
 
-        self.box = Box(self.pos, 5)
+        self.box = Box(self.pos, Vec2(5, 5))
 
     #here you will only change the movement direction vector and the position will update in step_frame
     def move(self):
@@ -71,7 +71,7 @@ class State:
         
         self.players = []
         for i in range(player_cnt):
-            self.players.append(Player((100, 100)))
+            self.players.append(Player(Vec2(100, 100)))
     
     def end_game(self):
         pass
@@ -80,9 +80,9 @@ class State:
         self.current_frame += 1
 
         player_cnt = 0
-        for player in players:
+        for player in self.players:
             #change position
-            player.pos += (movement[0] * player.speed, movement[1] * player.speed)
+            player.pos += player.speed * player.movement_dir
             player.box.pos = player.pos
 
             #check number of players alive
@@ -90,22 +90,20 @@ class State:
                 player_cnt+=1
             
         
-        for bullet in bullets:
+        for bullet in self.bullets:
             #change position
-            bullet.pos += (movement[0] * bullet.speed, movement[1] * bullet.speed)
+            bullet.pos += bullet.movement_dir * bullet.speed
             bullet.box.pos = bullet.pos
 
             #check collision with players
-            for player in players:
+            for player in self.players:
                 if Box.area_colliding(player.box, bullet.box):
                     player.hit()
                     bullet.kill()
 
 
         if player_cnt <= 1:
-            end_game()
-
-
+            self.end_game()
 
         
         
