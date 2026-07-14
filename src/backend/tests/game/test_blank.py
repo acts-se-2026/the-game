9423bf17eea616ac513ef7daacbc71beebb46055
RokasCy <rokas.rukuiza@gmail.com>
2026-07-14 11:49:02 +0300

    implemented testing for game logic

--- src/backend/tests/game/test_blank.py
+++ src/backend/tests/game/test_blank.py
@@ -1,5 +1,8 @@
+import pytest
 from game.state import Player, Box, State
+from game.state import PLAYER_SIZE, PLAYER_SPEED, SHOOTING_DELAY, BULLET_SPEED, BULLET_SIZE
 from game.vector import Vec2
+import math
 
 def test_collisions():
     box1 = Box(Vec2(0, 0), Vec2(5, 5))
@@ -22,6 +25,11 @@ def test_collisions():
 
     assert Box.area_colliding(box7, box8) == False
 
+def test_state_initializes_players():
+    state = State(["a", "b", "c"])
+
+    assert len(state.players) == 3
+
 
 def test_player_movemet():
     state = State(["p1", "p2"])
@@ -32,7 +40,7 @@ def test_player_movemet():
     player = state.players[0]
     player2 = state.players[1]
 
-    assert player.pos == Vec2(100, 100) + player.speed * Vec2(1, 0)
+    assert player.pos == Vec2(100, 100) + PLAYER_SPEED * Vec2(1, 0)
     assert player2.pos == Vec2(100, 100)
 
 
@@ -45,15 +53,90 @@ def test_player_hit():
     player.hit()
     assert player.hp == 90
 
-def test_player_hit():
-    state = State(["p1"])
+def test_shooting_direction():
+    player = Player("abc", Vec2(0, 0))
+    shooting_dir = player.get_shooting_dir()
 
+    assert shooting_dir.x == pytest.approx(1)
+    assert shooting_dir.y == pytest.approx(0)
+
+    player.rotation = math.pi / 2
+    shooting_dir = player.get_shooting_dir()
+
+    assert shooting_dir.x == pytest.approx(0)
+    assert shooting_dir.y == pytest.approx(1)
+
+    player.rotation = math.pi
+    shooting_dir = player.get_shooting_dir()
+
+    assert shooting_dir.x == pytest.approx(-1)
+    assert shooting_dir.y == pytest.approx(0)
+
+
+    player.rotation = 3 * math.pi / 2
+    shooting_dir = player.get_shooting_dir()
+
+    assert shooting_dir.x == pytest.approx(0)
+    assert shooting_dir.y == pytest.approx(-1)
+  
+def test_bullet_hits_player():
+    state = State(["a", "b"])
+
+    shooter = state.players[0]
+    target = state.players[1]
+
+    shooter.pos = Vec2(100, 100)
+    target.pos = Vec2(100 + BULLET_SPEED, 100)
+
+    shooter.rotation = 0
+
+    state.current_frame = SHOOTING_DELAY + 1
+    state.try_shoot_player_bullet("a")
+
+    state.step_frame()
+
+    assert target.hp == 90
+    assert len(state.bullets) == 0
+
+def test_bullet_delay():
+    state = State(["a"])
+    state.current_frame = 100
+
+    player =  state.players[0]
+    player.last_shot_time = 101
+
+    state.try_shoot_player_bullet("a")
+    assert len(state.bullets) == 0
+    assert player.last_shot_time == 101
+
+def test_collision_with_walls():
+    state = State(["a"])
     player = state.players[0]
 
-    assert player.hp == 100
-    player.hit()
-    assert player.hp == 90
+    # left wall
+    player.pos = Vec2(PLAYER_SIZE.x / 2, 50)
+    player.movement_dir = Vec2(-1, 0)
+    state.step_frame()
 
+    assert player.pos == Vec2(PLAYER_SIZE.x / 2, 50)
 
+    # right wall
+    player.pos = Vec2(state.level_size.x - PLAYER_SIZE.x / 2, 50)
+    player.movement_dir = Vec2(1, 0)
+    state.step_frame()
 
+    assert player.pos == Vec2(state.level_size.x - PLAYER_SIZE.x / 2, 50)
 
+    # top wall
+    player.pos = Vec2(50, PLAYER_SIZE.y / 2)
+    player.movement_dir = Vec2(0, -1)
+    state.step_frame()
+
+    assert player.pos == Vec2(50, PLAYER_SIZE.y / 2)
+
+    # bottom wall
+    player.pos = Vec2(50, state.level_size.y - PLAYER_SIZE.y / 2)
+    player.movement_dir = Vec2(0, 1)
+    state.step_frame()
+
+    assert player.pos == Vec2(50, state.level_size.y - PLAYER_SIZE.y / 2)
